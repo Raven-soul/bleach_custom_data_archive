ee07c9304d56b89ddc36273191de742bed9ce9b6
kozyrew.a <mr.kolin.199@mail.ru>
2023-09-06 14:29:52 +0300

    new db reader

--- Source/Js/main.js
+++ Source/Js/main.js
@@ -1,9 +1,12 @@
 function start() {
     debugger;
-    fetch("db_file.sqlite3")
+    fetch("db_bleach.sqlite3")
         .then((res) => res.text())
         .then((text) => {
-            alert(text)
+            let db = new SQL.Database(text);
+            let stmt = db.prepare("SELECT * FROM Class");
+            var row = stmt.get(1);
+            alert(row);
         })
         .catch((e) => console.error(e));
 }
