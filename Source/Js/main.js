285c578a3588069700f0d1271deaf7c3b06a3d14
kozyrew.a <mr.kolin.199@mail.ru>
2023-09-14 08:21:47 +0300

    начало работы с сайтом

--- Source/Js/main.js
+++ Source/Js/main.js
@@ -7,3 +7,18 @@ function start(){
         })
         .catch((e) => console.error(e));
 }
+
+function getCode(id_data){
+    fetch("data.txt")
+        .then((res) => res.text())
+        .then((text) => {
+            var mydata = JSON.parse(text);
+            return mydata.table[id_data].Id;
+        })
+        .catch((e) => console.error(e));
+    return null;
+}
+
+function fill(){
+    alert('ready');
+}
