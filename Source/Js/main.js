5ac17f8d6d50f5a78f75a19a37b4621e535749e5
kozyrew.a <mr.kolin.199@mail.ru>
2023-09-18 12:38:04 +0300

    попытка отрисовки окна с новостями

--- Source/Js/main.js
+++ Source/Js/main.js
@@ -1,13 +1,19 @@
-async function getTemplatesData() {
+async function awake(){
+    setChapterTitle("Онлайн-справочник Bleach D&D 5e");
+    setMainSectionDataChapterTitle("Новости сайта");
+    getTemplatesData(0);
+}
+
+async function getTemplatesData(blockTypeId) {
     var result;
-    fetch("Source/Json/menu_data_templates.json")
+    fetch("Source/Json/data_templates.json")
     .then((res) => res.text())
     .then(async (text) => { 
         var JSONdata = JSON.parse(text);
         getMenuContentData(JSONdata.templates); 
+        getInfoBlockContentData(JSONdata.templates, blockTypeId)
     })
     .catch((e) => console.error(e));
-    return result;
 }
 
 async function getMenuContentData(templatesData) {
@@ -18,14 +24,18 @@ async function getMenuContentData(templatesData) {
         var JSONdata = JSON.parse(text);
         mainMenuBuilder(JSONdata.mainMenuTableData, templatesData); 
     })
-    .catch((e) => console.error(e));
-    
+    .catch((e) => console.error(e));    
 }
 
-async function awake(){
-    setChapterTitle("Онлайн-справочник Bleach D&D 5e");
-    setMainSectionDataChapterTitle("Новости сайта");
-    getTemplatesData();
+async function getInfoBlockContentData(templatesData, blockTypeId) {
+    var result;
+    fetch("Source/Json/infoBlock_content_data.json")
+    .then((res) => res.text())
+    .then(async (text) => {
+        var JSONdata = JSON.parse(text);
+        mainInfoBlockContentBuilder(JSONdata.mainInfoBlockData[blockTypeId], templatesData); 
+    })
+    .catch((e) => console.error(e));    
 }
 
 function mainMenuBuilder (menuContent, templates){
@@ -54,4 +64,23 @@ function mainMenuBuilder (menuContent, templates){
     });
 
     menuBlock.html( summHtmlBlock );
+}
+
+function mainInfoBlockContentBuilder(infoBlockContent, templates){
+    var contentInfoBlockPosition = $(".main-info-data-section .row-2");
+    var summHtmlBlock = "";
+
+    infoBlockContent.List.forEach((blockElement) => {
+        let tempMenuStroke = templates[1].segments[0].data;
+        let tempMenuList = "";
+
+        tempMenuStroke = tempMenuStroke.replace("@@INFOBLOCKNAME@@", blockElement.Name);
+        tempMenuStroke = tempMenuStroke.replace("@@INFOBLOCKDATA@@", blockElement.Data);
+        tempMenuStroke = tempMenuStroke.replace("@@INFOBOCKIMAGE@@", blockElement.Image);
+        tempMenuStroke = tempMenuStroke.replace("@@INFOBLOCKFUNCTION@@", blockElement.Function);
+
+        summHtmlBlock = summHtmlBlock + tempMenuStroke;
+    });
+
+    contentInfoBlockPosition.html( summHtmlBlock );
 }
