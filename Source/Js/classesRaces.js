57ba11f37c9b9c844f01506078a74956cbecdf82
kozyrew.a <mr.kolin.199@mail.ru>
2023-09-19 09:25:01 +0300

    исправления 2

--- Source/Js/classesRaces.js
+++ Source/Js/classesRaces.js
@@ -2,36 +2,36 @@
 
 async function racesStartPage(){
     setChapterTitle("Рассы Bleach D&D 5e");
-    getTemplatesData(1);
+    getSecondContentTemplatesData(1);
 }
 
 // template functions
 
-async function getRacesTemplates(contentBlockTypeId) {
+async function getSecondContentTemplatesData(contentBlockTypeId) {
     var result;
     fetch("Source/Json/data_templates.json")
     .then((res) => res.text())
     .then(async (text) => { 
         var JSONdata = JSON.parse(text);
-        getBlockContentData(JSONdata.templates, contentBlockTypeId)
+        getSecondBlockContentData(JSONdata.templates, contentBlockTypeId)
     })
     .catch((e) => console.error(e));
 }
 
-async function getBlockContentData(templatesData, contentBlockTypeId) {
+async function getSecondBlockContentData(templatesData, contentBlockTypeId) {
     var result;
     fetch("Source/Json/infoBlock_content_data.json")
     .then((res) => res.text())
     .then(async (text) => {
         var JSONdata = JSON.parse(text);
-        mainInfoBlockContentBuilder(JSONdata.mainInfoBlockData[contentBlockTypeId], templatesData); 
+        RacesClassesContentBuilder(JSONdata.mainInfoBlockData[contentBlockTypeId], templatesData); 
     })
     .catch((e) => console.error(e));    
 }
 
 // content functions
 
-function mainInfoBlockContentBuilder(infoBlockContent, templates){
+function RacesClassesContentBuilder(infoBlockContent, templates){
     var contentInfoBlockPosition = $(".main-info-data-section .row-2");
     var summHtmlBlock = "";
     var gridHtmlBlock = templates[2].segments[0].data;
