7d114e4f8822bcf1703859c3dfec469e9fbbbf85
kozyrew.a <mr.kolin.199@mail.ru>
2023-09-20 15:05:23 +0300

    разделение рас и классов

--- Source/Js/classesRaces.js
+++ Source/Js/classesRaces.js
@@ -1,38 +1,62 @@
 // start generation functions
 
 async function racesStartPage(){
-    setChapterTitle("Рассы Bleach D&D 5e");
-    setMainSectionDataChapterTitle("", false);
-    getSecondContentTemplatesData(1);
+    getRacesContentTemplatesData();
 }
 
-// template functions
+async function classesStartPage(){
+    getClassesContentTemplatesData();
+}
 
-async function getSecondContentTemplatesData(contentBlockTypeId) {
+// template and content data functions
+
+async function getRacesContentTemplatesData() {
     var result;
     fetch("Source/Json/data_templates.json")
     .then((res) => res.text())
     .then(async (text) => { 
         var JSONdata = JSON.parse(text);
-        getSecondBlockContentData(JSONdata.templates, contentBlockTypeId)
+        getRacesContentData(JSONdata.templates)
     })
     .catch((e) => console.error(e));
 }
 
-async function getSecondBlockContentData(templatesData, contentBlockTypeId) {
+async function getClassesContentTemplatesData() {
+    var result;
+    fetch("Source/Json/data_templates.json")
+    .then((res) => res.text())
+    .then(async (text) => { 
+        var JSONdata = JSON.parse(text);
+        getClassesContentData(JSONdata.templates)
+    })
+    .catch((e) => console.error(e));
+}
+
+async function getRacesContentData(templatesData) {
     var result;
     fetch("Source/Json/infoBlock_content_data.json")
     .then((res) => res.text())
     .then(async (text) => {
         var JSONdata = JSON.parse(text);
-        RacesClassesContentBuilder(JSONdata.mainInfoBlockData[contentBlockTypeId], templatesData); 
+        RacesWebContentBuilder(JSONdata.mainInfoBlockData[1], templatesData); 
     })
     .catch((e) => console.error(e));    
 }
 
-// content functions
+async function getClassesContentData(templatesData) {
+    var result;
+    fetch("Source/Json/infoBlock_content_data.json")
+    .then((res) => res.text())
+    .then(async (text) => {
+        var JSONdata = JSON.parse(text);
+        ClassesWebContentBuilder(JSONdata.mainInfoBlockData[2], templatesData); 
+    })
+    .catch((e) => console.error(e));    
+}
 
-function RacesClassesContentBuilder(infoBlockContent, templates){
+// web content functions
+
+function RacesWebContentBuilder(infoBlockContent, templates){
     var contentInfoBlockPosition = $(".main-info-data-section .row-2");
     var summHtmlBlock = "";
     var gridHtmlBlock = templates[2].segments[0].data;
@@ -62,5 +86,44 @@ function RacesClassesContentBuilder(infoBlockContent, templates){
     gridHtmlBlock = gridHtmlBlock.replace("@@INFOBLOCKGRIDDATA@@", summHtmlBlock);
     
     contentInfoBlockPosition.html( gridHtmlBlock );
+
     checkMobileMenuData();
+    setChapterTitle(infoBlockContent.Name);
+    setMainSectionDataChapterTitle("", false);
+}
+
+function ClassesWebContentBuilder(infoBlockContent, templates){
+    var contentInfoBlockPosition = $(".main-info-data-section .row-2");
+    var summHtmlBlock = "";
+    var gridHtmlBlock = templates[2].segments[0].data;
+
+    infoBlockContent.Group.forEach((groupBlockElement) => {
+        var gridGroupHtmlBlock = templates[2].segments[1].data;
+
+        gridGroupHtmlBlock = gridGroupHtmlBlock.replace("@@INFOBLOCKGRIDGROUPNAME@@", groupBlockElement.GroupName);
+        
+        var tempGroupList = ""
+        groupBlockElement.List.forEach((itemElement) => {
+            let tempGroupStroke = templates[2].segments[2].data;
+
+            tempGroupStroke = tempGroupStroke.replace("@@INFOBLOCKCLASSFUNCTION@@", itemElement.Function);
+            tempGroupStroke = tempGroupStroke.replace("@@INFOBLOCKCLASSIMAGE@@", itemElement.Image);
+            tempGroupStroke = tempGroupStroke.replace("@@INFOBLOCKCLASSHEADNAME@@", itemElement.HeadName);
+            tempGroupStroke = tempGroupStroke.replace("@@INFOBLOCKCLASSAPPENDNAME@@", itemElement.AppendName);
+
+            tempGroupList = tempGroupList + tempGroupStroke;
+        });
+
+        gridGroupHtmlBlock = gridGroupHtmlBlock.replace("@@INFOBLOCKGRIDGROUPDATA@@", tempGroupList);
+
+        summHtmlBlock = summHtmlBlock + gridGroupHtmlBlock;
+    });
+
+    gridHtmlBlock = gridHtmlBlock.replace("@@INFOBLOCKGRIDDATA@@", summHtmlBlock);
+    
+    contentInfoBlockPosition.html( gridHtmlBlock );
+
+    checkMobileMenuData();
+    setChapterTitle(infoBlockContent.Name);
+    setMainSectionDataChapterTitle("", false);
 }
